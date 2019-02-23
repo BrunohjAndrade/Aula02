d81e2113f6fbf6968033ac980030946d6e4cd6cf
Bruno Henrique <brunothedark13@gmail.com>
2019-02-22 21:14:44 -0300

    Aplicação altera texto de campo para label

--- app/src/main/java/i/pdm/geradornumeroaleatorios/MainActivity.java
+++ app/src/main/java/i/pdm/geradornumeroaleatorios/MainActivity.java
@@ -2,6 +2,9 @@ package i.pdm.geradornumeroaleatorios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void gerarValor(View view) {
+        TextView l = (TextView) findViewById(R.id.texto);
+        EditText campo = (EditText)findViewById(R.id.campo);
+        l.setText(campo.getText());
+        campo.setText("");
+
+
     }
 }
